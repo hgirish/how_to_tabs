45952be0e768c7a2b68bc5bef4e216bbd8c5490a
Girish <girish@post.com>
2023-03-16 21:09:03 -0700

    jake sh

--- jake.sh
+++ jake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[! -f node_modules/.bin/jake ] && echo "Building npm modules: " && npm rebuild
+[ ! -f node_modules/.bin/jake ] && echo "Building npm modules:" && npm rebuild
 
 node_modules/.bin/jake $*
 
