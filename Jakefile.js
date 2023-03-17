54aa02422b9b69fa72dd5c825c5cb854c931a5ab
Girish <girish@post.com>
2023-03-16 21:17:09 -0700

    semver

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,24 @@
 (function () {
     "use strict";
 
+    var semver = require('semver');
+
     desc("Deault build");
-    task("default", function () {
+    task("default", ["version"], function () {
         console.log("\n\nBUILD OK");
     });
+
+    desc("Check Node version")
+    task("version", function () {
+        console.log("Checking node version: .");
+
+        var packageJson = require("./package.json");
+        var expectedVersion = packageJson.engines.node;
+
+        var actualVersion = process.version;
+        if (semver.neq(expectedVersion, actualVersion)) {
+            fail("Incorrect Node version: expected " + expectedVersion + ", but was " + actualVersion)
+        }
+    })
 
 }());
