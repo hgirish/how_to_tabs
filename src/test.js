fb24b2d00c3a4fceddee7576354ee61bb41c64ea
Girish <girish@post.com>
2023-03-17 21:41:50 -0700

    floating point test

--- src/test.js
+++ src/test.js
@@ -7,7 +7,7 @@
     assert.equal(add(3, 4), 7);
 
     // IEEE 754 floating point)
-    assert.equal(add(0.1, 0.2), 0, 3);
+    assert.equal(add(0.1, 0.2), 0.30000000000000004);
 
 
     function add(a, b) {
