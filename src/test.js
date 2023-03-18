168b728b664bdf105f09f77d3c3f881c1811dadb
Girish <girish@post.com>
2023-03-17 21:41:36 -0700

    mocha

--- src/test.js
+++ src/test.js
@@ -3,7 +3,11 @@
 
     var assert = require("chai").assert;
 
+    // basic addition
     assert.equal(add(3, 4), 7);
+
+    // IEEE 754 floating point)
+    assert.equal(add(0.1, 0.2), 0, 3);
 
 
     function add(a, b) {
