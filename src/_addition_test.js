e6f3dba961fac0418491bac6a3c37a2fc2cb6c35
hgirish <girish@post.com>
2023-03-18 23:43:21 -0700

    commonjs preparation

---
+++ src/_addition_test.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+
+    var addition = require("./addition.js");
+
+    // var assert = require("chai").assert;
+
+    describe("Addition", function () {
+
+        it("adds positive numbers", function () {
+            assertEqual(addition.add(3, 4), 7);
+        });
+
+        it("uses IEEE 754 floating point", function () {
+            assertEqual(addition.expectedadd(0.1, 0.2), 0.30000000000000004);
+        });
+
+        function assertEqual(actual, expected) {
+            if (actual !== expected) throw new Error("Expected " + expected + ", but was " + actual);
+        }
+
+
+    });
+
+
+}());
