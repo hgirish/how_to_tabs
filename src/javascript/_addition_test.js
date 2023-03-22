a5ef3e3a7490bfb2c1a5aa3e447c02c705ca5916
Girish <girish@post.com>
2023-03-21 20:49:13 -0700

    dom manipulation

--- src/javascript/_addition_test.js
+++ src/javascript/_addition_test.js
@@ -6,6 +6,19 @@
     describe("Something", function () {
 
         it("something", function () {
+            var div = document.createElement("div");
+
+            div.innerHTML = "This is an example div.";
+
+            document.body.appendChild(div);
+
+            var p = document.createElement("p");
+
+            p.innerHTML = "This is an embedded paragraph";
+
+            div.appendChild(p);
+
+            div.parentNode.removeChild(div);
         });
 
     });
