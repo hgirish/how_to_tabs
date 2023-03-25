01f8a39537710bdd0174730deaf93fd28ae3a360
Girish <girish@post.com>
2023-03-24 21:14:39 -0700

    options parameter

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -2,10 +2,18 @@
 (function () {
     "use strict";
 
-    exports.initialize = function initialize(elementList, className) {
-        elementList.forEach(element => {
-            element.classList.add(className);
+    exports.initialize = function initialize(options) {
+        const { content: content, defaultElement, contentHideClass: contentHideClass } = options;
+
+        if (content === undefined) throw new Error("expected options.content");
+        if (defaultElement === undefined) throw new Error("expected options.defaultElement");
+        if (contentHideClass === undefined) throw new Error("expected options.contentHideClass");
+
+
+        content.forEach(element => {
+            element.classList.add(contentHideClass);
         });
+        defaultElement.classList.remove(contentHideClass);
     };
 
 })();
